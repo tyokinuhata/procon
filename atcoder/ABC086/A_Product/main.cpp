7de76327ad704768f26b24d4091fcfd98bb2d464
Kazuki Tobita <kazukiti201@gmail.com>
2018-03-22 06:28:59 +0900

    update ABC086 A

--- atcoder/ABC086/A_Product/main.cpp
+++ atcoder/ABC086/A_Product/main.cpp
@@ -7,9 +7,6 @@ int main () {
     int a, b;
     cin >> a >> b;
 
-    if (a * b % 2 == 0) {
-        cout << "Even" << endl;
-    } else {
-        cout << "Odd" << endl;
-    }
+    if (a * b % 2 == 0) cout << "Even" << endl;
+    else cout << "Odd" << endl;
 }
