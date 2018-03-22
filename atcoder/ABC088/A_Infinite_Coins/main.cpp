6bf0e1339122e0bbf5d212cdbb1e9c785319b3fd
Kazuki Tobita <kazukiti201@gmail.com>
2018-03-22 17:27:13 +0900

    update ABC088 A

--- atcoder/ABC088/A_Infinite_Coins/main.cpp
+++ atcoder/ABC088/A_Infinite_Coins/main.cpp
@@ -7,6 +7,5 @@ int main () {
     int n, a;
     cin >> n >> a;
 
-    if (n % 500 <= a) cout << "Yes" << endl;
-    else cout << "No" << endl;
+    cout << (n % 500 <= a ? "Yes" : "No") << endl;
 }
