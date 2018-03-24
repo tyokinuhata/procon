4f05bb651f584b8ec6ff96fc2e8e68a7d4ca4a73
Kazuki Tobita <kazukiti201@gmail.com>
2018-03-24 23:41:53 +0900

    solved ARC004 A

---
+++ atcoder/ARC004A_The_longest_distance.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int main () {
+    int n, x[100], y[100];
+
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        cin >> x[i] >> y[i];
+    }
+
+    double distance, max = 0;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            distance = sqrt(pow(x[j] - x[i], 2) + pow(y[j] - y[i], 2));
+            if (distance > max) max = distance;
+        }
+    }
+
+    cout << max << endl;
+}
