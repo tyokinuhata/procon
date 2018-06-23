ef61414ac5fe2fd5508f406c23882dc68a1d1958
Kazuki Tobita <kazukiti201@gmail.com>
2018-06-23 20:51:52 +0900

    solved ITP1 3 D

---
+++ AOJ/ITP103D_How_Many_Divisors.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main () {
+    int a, b, c, cnt = 0;
+
+    cin >> a >> b >> c;
+
+    for (int i = a; i <= b; i++) {
+        if (c % i == 0) cnt++;
+    }
+
+    cout << cnt << endl;
+}
