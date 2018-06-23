208b737e1b42b371f721a72d3627c942988a1c15
Kazuki Tobita <kazukiti201@gmail.com>
2018-06-24 00:03:35 +0900

    solved ITP1 4 D

---
+++ AOJ/ITP104D_Min_Max_and_Sum.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main () {
+    int n, a, min = 1000000, max = -1000000;
+    long long sum = 0;
+
+    cin >> n;
+
+    for (int i = 0; i < n; i++) {
+        cin >> a;
+
+        if (a > max) max = a;
+        if (a < min) min = a;
+
+        sum += a;
+    }
+    cout << min << " " << max << " " << sum << endl;
+}
