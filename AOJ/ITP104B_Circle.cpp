d68cf1b9e2a8953e0886dfc5be3b5ef555fb9fd1
Kazuki Tobita <kazukiti201@gmail.com>
2018-06-23 23:42:25 +0900

    solved ITP1 4 B

---
+++ AOJ/ITP104B_Circle.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int main () {
+    double r;
+
+    cin >> r;
+
+    cout << fixed << r * r * M_PI << " " << 2 * r * M_PI << endl;
+}
