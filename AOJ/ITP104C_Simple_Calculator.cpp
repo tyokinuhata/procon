597da59c2d0ab8cfd2c3d9e495c2665c530a354f
Kazuki Tobita <kazukiti201@gmail.com>
2018-06-23 23:49:52 +0900

    solved ITP1 4 C

---
+++ AOJ/ITP104C_Simple_Calculator.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main () {
+    int a, b;
+    char op;
+
+    while (true) {
+        cin >> a >> op >> b;
+        
+        if (op == '?') break;
+
+        if (op == '+') {
+            cout << a + b << endl;
+        } else if (op == '-') {
+            cout << a - b << endl;
+        } else if (op == '*') {
+            cout << a * b << endl;
+        } else {
+            cout << a / b << endl;
+        }
+    }
+}
